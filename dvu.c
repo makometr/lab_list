3f7ab30ee8b7b4088eca168574e40eebc7e31c1c
makometr <makometr@yandex.ru>
2017-03-16 01:41:34 +0300

    Добавлено новое окно, изменены переменные списка - создана функция чтения из файла и иницилизации списка int create_list возвращает количество прочитанных записей) - проверка на правильность читаемых данных не требутеся, т.к. работа с файлом производится по стороны программы - новое окно addMenu для добавления новых записей

--- dvu.c
+++ dvu.c
@@ -9,22 +9,27 @@ char *delete_enter(char*);
 void refresh_active_menu(WINDOW*, int active_string, char menu_list[MAX_LIST_MENU][16]);
 unsigned int logs_out(WINDOW*, unsigned int, char*);
 
-typedef struct person
+typedef struct paper
 {
 	char name[255];
-	int age;
-	struct person *next;
-	struct person *prev;
-} PERSON;
+	int price;
+	struct paper *next;
+	struct paper *prev;
+} PAPER;
+
+int create_list(FILE *f,PAPER **HEAD);
+
 
 int main(){
 	char menu_list[MAX_LIST_MENU][16] = {"Add record", "Delete record", "Save changes", "Quit"};
-	WINDOW *menu, *logs, *list;
-	int start_x_menu, start_x_logs, start_x_list;
-	int start_y_menu, start_y_logs, start_y_list;
-	int w_menu, w_logs, w_list;
-	int h_menu, h_logs, h_list;
+	WINDOW *menu, *logs, *list, *addMenu;
+	PAPER* HEAD = NULL;
+	int start_x_menu, start_x_logs, start_x_list, start_x_addMenu;
+	int start_y_menu, start_y_logs, start_y_list, start_y_addMenu;
+	int w_menu, w_logs, w_list, w_addMenu;
+	int h_menu, h_logs, h_list, h_addMenu;
 
+	unsigned int y_log_curr = 0;
 	int active_string = 1;
 	int key;
 
@@ -52,19 +57,26 @@ int main(){
 	w_list = w_logs;
 	h_list = LINES;
 
+	start_x_addMenu = start_x_menu;
+	start_y_addMenu = start_y_menu + h_menu + 1;
+	w_addMenu = w_menu;
+	h_addMenu = h_menu + 2;
+
 	menu = newwin(h_menu, w_menu, start_y_menu, start_x_menu);
 	logs = newwin(h_logs, w_logs, start_y_logs, start_x_logs);
 	list = newwin(h_list, w_list, start_y_list, start_x_list);
+	addMenu = newwin(h_addMenu, w_addMenu, start_y_addMenu, start_x_addMenu);
 	refresh();
 
 	wbkgdset(menu, COLOR_PAIR(1));
 	wclear(menu);
 	box(menu, 0, 0);
 	mvwprintw(menu,0,8,"MENU");
-	mvwprintw(menu,2,4,"%s", menu_list[0]);
-	mvwprintw(menu,4,4,"%s", menu_list[1]);
-	mvwprintw(menu,6,4,"%s", menu_list[2]);
-	mvwprintw(menu,8,4,"%s", menu_list[3]);
+	mvwprintw(menu,2,3,"%s", menu_list[0]);
+	mvwprintw(menu,4,3,"%s", menu_list[1]);
+	mvwprintw(menu,6,3,"%s", menu_list[2]);
+	mvwprintw(menu,8,3,"%s", menu_list[3]);
+	refresh_active_menu(menu, active_string, menu_list);
 	wrefresh(menu);
 
 	wbkgdset(logs, COLOR_PAIR(2));
@@ -73,22 +85,39 @@ int main(){
 	mvwprintw(logs,0,12,"LOGS");
 	wrefresh(logs);
 
-
 	wbkgdset(list, COLOR_PAIR(1));
 	wclear(list);
 	box(list, 0, 0);
 	mvwprintw(list,0,12,"LIST");
 	wrefresh(list);
 
-	unsigned int y_log_curr = 0;
+	wbkgdset(addMenu, COLOR_PAIR(2));
+	wclear(addMenu);
+	box(addMenu, 0, 0);
+	mvwprintw(addMenu,0,5,"ADD RECORD");
+	mvwprintw(addMenu,2,3,"Name of Paper:");
+	mvwprintw(addMenu,4,3,"_");
+	mvwprintw(addMenu,6,3,"Price:");
+	mvwprintw(addMenu,8,3,"_");
+	mvwprintw(addMenu,10,1,"Press Enter to add");
+	wrefresh(addMenu);
+
 	wattron(logs, COLOR_PAIR(2));
 	FILE *f = fopen("source.txt", "r");
 	if (f == NULL){
 		y_log_curr = logs_out(logs, y_log_curr, "Openning file... Error.");
 		// return 0;
 	}
-	else 
+	else {
 		y_log_curr = logs_out(logs, y_log_curr, "Openning file... Complete.");
+		int lol = create_list(f, &HEAD);
+		mvwprintw(list,3,1,"%s", HEAD->name);
+		wrefresh(list);
+		if (lol == 0)
+			y_log_curr = logs_out(logs, y_log_curr, "Problem with data reading");
+		else 
+			y_log_curr = logs_out(logs, y_log_curr, "List has been created");
+	}
 
 	while((key = getch()) != KEY_F(1)){
         switch(key){	
@@ -111,30 +140,7 @@ int main(){
 	}
 
 	/*
-	PERSON *head = NULL;
-	PERSON *current = NULL;
-	int i = 0;
-	int data_error = 0;
 
-	while (feof(f) == 0){
-		PERSON *tmp = (PERSON*)malloc(sizeof(PERSON));
-		fgets(tmp->name, 255, f);
-		strcpy(tmp->name, delete_enter(tmp->name));
-		if (fscanf(f, "%d\n", &tmp->age) == EOF){
-			printw("Problem with data-reading. Check file source.txt\n");
-			data_error = 1;
-		}
-		if (i++ == 0){ 
-			head = tmp;
-			tmp->prev = NULL;
-		}
-		else {
-			current->next = tmp;
-			tmp->prev = current;
-		}
-		current = tmp;
-		tmp->next = NULL;
-	}
 
 	printw("Closing file... ");
 	if (fclose(f) != 0)
@@ -151,21 +157,21 @@ int main(){
 	printw("Press the left-arrow key to see the next element.\n");
 	PERSON *ptr = head;
 	i = 0;
-	printw("%i) Name: %s Age: %d\n", ++i, ptr->name, ptr->age);
+	printw("%i) Name: %s price: %d\n", ++i, ptr->name, ptr->price);
 	int key;
 	while((key = getch()) != KEY_F(1)){
         switch(key){	
             case KEY_RIGHT:
 				if (ptr->next != NULL){
 					ptr = ptr->next;
-					printw("%i) Name: %s Age: %d\n", ++i, ptr->name, ptr->age);
+					printw("%i) Name: %s price: %d\n", ++i, ptr->name, ptr->price);
 				}
 				else printw("The end of data.\n");
 				break;
 			case KEY_LEFT:
 				if (ptr->prev != NULL){
 					ptr = ptr->prev;
-					printw("%i) Name: %s Age: %d\n", --i, ptr->name, ptr->age);
+					printw("%i) Name: %s price: %d\n", --i, ptr->name, ptr->price);
 				}
 				else printw("The end of data.\n");	
 			default:
@@ -174,7 +180,7 @@ int main(){
 	}
 	/*
 	while (ptr){
-		printw("Name: %s Age: %d\n", ptr->name, ptr->age);
+		printw("Name: %s price: %d\n", ptr->name, ptr->price);
 		ptr = ptr->next;
 	}
 	*/
@@ -191,14 +197,12 @@ char* delete_enter(char* string){
 
 void refresh_active_menu(WINDOW *WIN, int active,char menu_list[MAX_LIST_MENU][16]){
 	wattron(WIN, COLOR_PAIR(1));
-	mvwprintw(WIN,2,4,"%s", menu_list[0]);
-	mvwprintw(WIN,4,4,"%s", menu_list[1]);
-	mvwprintw(WIN,6,4,"%s", menu_list[2]);
-	mvwprintw(WIN,8,4,"%s", menu_list[3]);
+	mvwprintw(WIN,2,3,"%s", menu_list[0]);
+	mvwprintw(WIN,4,3,"%s", menu_list[1]);
+	mvwprintw(WIN,6,3,"%s", menu_list[2]);
+	mvwprintw(WIN,8,3,"%s", menu_list[3]);
 	wattron(WIN, COLOR_PAIR(2));
-	mvwprintw(WIN,active*2,4,menu_list[active-1]);
-	
-
+	mvwprintw(WIN,active*2,3,menu_list[active-1]);
 	wrefresh(WIN);
 	wattron(WIN, COLOR_PAIR(1));
 }
@@ -216,4 +220,29 @@ unsigned int logs_out(WINDOW* WIN, unsigned int y_log_curr ,char* string){
 	mvwprintw(WIN,y_log_curr*2, 1, "%s", string);
 	wrefresh(WIN);
 	return y_log_curr;
+}
+
+int create_list(FILE *f, PAPER **head){
+	(*head) = NULL;
+	PAPER *current = NULL;
+	int i = 0;
+
+	while (feof(f) == 0){
+		PAPER *tmp = (PAPER*)malloc(sizeof(PAPER));
+		fscanf(f, "%s\n", &tmp->name);
+		fscanf(f, "%d\n", &tmp->price);
+			//printf("Problem with data-reading. Check file source.txt\n");
+		if (i++ == 0){ 
+			(*head) = tmp;
+			tmp->prev = NULL;
+		}
+		else {
+			current->next = tmp;
+			tmp->prev = current;
+		}
+		current = tmp;
+		tmp->next = (*head);
+		(*head)->prev = current;
+	}
+	return i;
 }
